bded632760a9c265387f00f3fd9a1e96c8ad3da4
Mohammed-ux <59700004+Mohammed-ux@users.noreply.github.com>
2020-06-18 15:40:26 -0700

    upload app

---
+++ script.js
@@ -0,0 +1,5 @@
+const navbtn=document.getElementById("nav-btn");
+const navbar=document.getElementById("nav-links");
+navbtn.addEventListener('click',()=>{
+navbar.classList.toggle("show-links");
+});
